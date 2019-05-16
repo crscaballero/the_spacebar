a299c0e2f93eead93c8497d16c7142c85c6375d6
cristian-hottomali <cristian@hottomali.com>
2019-05-16 16:35:30 -0700

    add twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,21 +4,33 @@ namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ArticleController{
+class ArticleController extends AbstractController{
 
 	/**
 	* @Route("/")
 	*/
-	public function homepage(){
+	public function homepage()
+	{
 		return new Response('OMG! my first page already! Woo!');
 	}
 
 	/**
 	* @Route("/news/{slug}")
 	*/
-	public function show($slug){
-		return new Response(sprintf('Future page to the article: %s', $slug));
+	public function show($slug)
+	{
+		$comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
+		return $this->render('article/show.html.twig', [
+			'title' => ucwords(str_replace('-', ' ', $slug)),
+			'comments' => $comments,
+		]);
 	}
 
 }
