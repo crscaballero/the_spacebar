e4012f3476e68346456cf064c7a3d2e07b55f78e
cristian-hottomali <cristian@hottomali.com>
2019-05-17 10:06:54 -0700

    add js function

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,19 +5,20 @@ namespace App\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ArticleController extends AbstractController{
 
 	/**
-	* @Route("/")
+	* @Route("/", name="app_homepage")
 	*/
 	public function homepage()
 	{
-		return new Response('OMG! my first page already! Woo!');
+		return $this->render('article/homepage.html.twig');
 	}
 
 	/**
-	* @Route("/news/{slug}")
+	* @Route("/news/{slug}", name="aticle_show")
 	*/
 	public function show($slug)
 	{
@@ -29,8 +30,18 @@ class ArticleController extends AbstractController{
 
 		return $this->render('article/show.html.twig', [
 			'title' => ucwords(str_replace('-', ' ', $slug)),
+			'slug' => $slug,
 			'comments' => $comments,
 		]);
+	}
+
+	/**
+	 * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+	*/
+	public function toggleArticleHeart($slug)
+	{
+		//TODO - Acctually hear/unhear the article!
+		return new JsonResponse(['hearts' => rand(5, 100)]);
 	}
 
 }
