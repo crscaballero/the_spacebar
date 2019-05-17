8bee96734a7159adca17dbf057bca8d0195a76a9
cristian-hottomali <cristian@hottomali.com>
2019-05-17 09:13:06 -0700

    improve template

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -25,7 +25,7 @@ class ArticleController extends AbstractController{
             'I ate a normal rock once. It did NOT taste like bacon!',
             'Woohoo! I\'m going on an all-asteroid diet!',
             'I like bacon too! Buy some from my site! bakinsomebacon.com',
-        ];
+		];
 
 		return $this->render('article/show.html.twig', [
 			'title' => ucwords(str_replace('-', ' ', $slug)),
